b1fa91d82a204f11d0fc0eb892eae92dfa2545de
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 22:06:57 +0530

    Update attendance.py

--- routes/attendance.py
+++ routes/attendance.py
@@ -39,4 +39,5 @@ async def search_by_date(
         date: str = Form(...),
         token: str = Depends(oauth2_scheme)
 ):
-    
+    if await get_current_user(token) is None:
+        raise credentials_exception
