81d593c249bde78a156c579a0029d5e78547f93e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:57:42 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -2,7 +2,7 @@ import mysql.connector
 from mysql.connector import errorcode
 
 """
-    middleware for accessing the attendance database and performing CRUD operations on the user table
+    middleware for accessing the attendance database and performing CRUD operations on the student table
 """
 
 
