a5d2ac467e7827d6b1504c378435dcfef92b8e4e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:05:39 +0530

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -3,7 +3,8 @@ from pydantic import BaseModel
 
 class User(BaseModel):
     id: int
-    username: str | None = None
+    firstname: str | None = None
+    lastname: str | None = None
     email: str | None = None
     is_admin: bool | None = None
 
