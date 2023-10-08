f01cbaf93950dbf0993610b79f9eea5366f2d69e
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:41:31 +0530

    Update student_model.py

--- models/student_model.py
+++ models/student_model.py
@@ -1,13 +1,4 @@
 class Student:
-    def __init__(self, i, n, a, g, d, p, c, gr):
-        self.index_number = i
-        self.name = n
-        self.address = a
-        self.gender = g
-        self.date_of_birth = d
-        self.parent_name = p
-        self.contact_number = c
-        self.grade = gr
 
     # index_number: str
     # name: str | None = None
@@ -17,3 +8,12 @@ class Student:
     # parent_name: str | None = None
     # contact_number: str | None = None
     # grade: str | None = None
+    def __init__(self, i, n, a, g, d, p, c, gr):
+        self.index_number = i
+        self.name = n
+        self.address = a
+        self.gender = g
+        self.date_of_birth = d
+        self.parent_name = p
+        self.contact_number = c
+        self.grade = gr
