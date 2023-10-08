2dd324843a595c6e6c81a10839a9403248c35ecc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:17:43 +0530

    Update student.py

--- routes/student.py
+++ routes/student.py
@@ -6,4 +6,8 @@ from fastapi import APIRouter, Form, HTTPException, status, Depends
 from auth.authorize import authenticate_user, oauth2_scheme
 from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
 
-
+router = APIRouter(
+    prefix="/api/student",
+    tags=["auth"],
+    responses={404: {"description": "The requested page was not found"}},
+)
