e17fc4e4cbad8166f8fb4c05bfc993314cd73941
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:17:27 +0530

    Update student.py

---
+++ routes/student.py
@@ -0,0 +1,7 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Form, HTTPException, status, Depends
+
+from auth.authorize import authenticate_user, oauth2_scheme
+from auth.hashing import get_password_hash, ACCESS_TOKEN_EXPIRE_MINUTES, create_access_token, blacklist_token
