f88c58bb98a051dd56cf4d08113905ee21887b49
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:08:56 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -61,7 +61,7 @@ async def register_user(
     Raises:
         HTTPException: if the username already exists
     """
-    if user_exists(username):
+    if user_exists(email):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Username already exists",
