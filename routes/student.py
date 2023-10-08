2925acd83cd5211c88851cee652556fe7a6f571f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:24:26 +0530

    Update student.py

--- routes/student.py
+++ routes/student.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends, Form
 
 from auth.authorize import oauth2_scheme, get_current_user, credentials_exception
-from services.student_service import add_student
+from services.student_service import add_student, get_student
 
 router = APIRouter(
     prefix="/api/student",
