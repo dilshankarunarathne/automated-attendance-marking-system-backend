043059c46b2c1e0a66102ab2adfc4c6913726f62
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:01:41 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -37,7 +37,7 @@ class StudentDAO:
     def register_student(self):
         cursor = self.cnx.cursor()
         add_user = ("INSERT INTO student "
-                    "(index_number, name, address, gender, date_of_birth, parent_name, contact_number) "
+                    "(index_number, name, address, gender, date_of_birth, parent_name, contact_number, grade) "
                     "VALUES (%s, %s, %s, %s, %s, %s)")
         data_student = (user.firstname, user.lastname, user.email, user.contact_number, user.is_admin, user.hashed_password)
         cursor.execute(add_user, data_student)
