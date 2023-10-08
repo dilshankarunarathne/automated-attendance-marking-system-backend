d6d019ca567b773932a96c6b763be2de080eb786
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 13:57:06 +0530

    Update authorize.py

--- auth/authorize.py
+++ auth/authorize.py
@@ -39,7 +39,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
         username: str = payload.get("sub")
         if username is None:
             raise credentials_exception
-        token_data = TokenData(username=username)
+        token_data = TokenData(email=username)
     except JWTError:
         raise credentials_exception
     user = get_user(email=token_data.email)
