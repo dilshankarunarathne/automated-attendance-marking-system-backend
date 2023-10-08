3fba85c02487948852dc9f070a7e507c64ff1820
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:51:16 +0530

    Update student_model.py

--- models/student_model.py
+++ models/student_model.py
@@ -4,7 +4,7 @@ from pydantic import BaseModel
 class Student(BaseModel):
     index_number: str
     name: str | None = None
-    adress: str | None = None
+    address: str | None = None
     gender: str | None = None
     date_of_birth: str | None = None
     parent_name: str | None = None
