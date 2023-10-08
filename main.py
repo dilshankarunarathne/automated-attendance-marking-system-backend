60ad0c47fb53134aa0526fc2d8133dd4ef9fb102
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 13:05:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,15 @@ from routes import auth
 
 app = FastAPI()
 
+origins = ['http://localhost:3000', 'http://192.168.178.23:3000']  # add your front-end ip:port here
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth.router)
 
