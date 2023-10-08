601d1b59efd045e7379c218a2b7d1995903feee5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:57:13 +0530

    Update student_dao.py

---
+++ dao/student_dao.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+
+import mysql.connector
+from mysql.connector import errorcode
+
+from models.user_model import User, UserInDB
+
+"""
+    middleware for accessing the user database and performing CRUD operations on the user table
+"""
+
+
+class UserDAO:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.cnx = None
+
+    def connect(self):
+        try:
+            self.cnx = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print("Something is wrong with your user name or password")
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print("Database does not exist")
+            else:
+                print(err)
+
+    def disconnect(self):
+        if self.cnx is not None:
+            self.cnx.close()
+            
