09ecad9ea2a04310c42fdc32c3e73de108fe0e5d
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 21:41:20 +0530

    Update attendance_model.py

--- models/attendance_model.py
+++ models/attendance_model.py
@@ -2,6 +2,6 @@ class Attendance:
     student_index_number: str
     date: str
 
-    def __init__(self):
+    def __init__(self, index):
         self.student_index_number = index
         self.date = d
