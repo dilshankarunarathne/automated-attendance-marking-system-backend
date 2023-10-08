1f77f6ee38289fe14bf8e1a31e4d39945a5fe82b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:39:12 +0530

    Update student_model.py

--- models/student_model.py
+++ models/student_model.py
@@ -1,13 +1,4 @@
 class Student:
-    # index_number: str
-    # name: str | None = None
-    # address: str | None = None
-    # gender: str | None = None
-    # date_of_birth: str | None = None  # Date
-    # parent_name: str | None = None
-    # contact_number: str | None = None
-    # grade: str | None = None
-
     def __int__(self, i, n, a, g, d, p, c, gr):
         self.index_number = i
         self.name = n
@@ -17,3 +8,14 @@ class Student:
         self.parent_name = p
         self.contact_number = c
         self.grade = gr
+        
+    # index_number: str
+    # name: str | None = None
+    # address: str | None = None
+    # gender: str | None = None
+    # date_of_birth: str | None = None  # Date
+    # parent_name: str | None = None
+    # contact_number: str | None = None
+    # grade: str | None = None
+
+
