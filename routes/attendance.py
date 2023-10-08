c0ff8368c389dc3afc797cb18361eb7befb1d3a0
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 22:01:27 +0530

    Update attendance.py

--- routes/attendance.py
+++ routes/attendance.py
@@ -4,7 +4,7 @@ from auth.authorize import oauth2_scheme, get_current_user, credentials_exceptio
 from services.student_service import add_student, get_student
 
 router = APIRouter(
-    prefix="/api/student",
+    prefix="/api/attendance",
     tags=["student"],
     responses={404: {"description": "The requested page was not found"}},
 )
