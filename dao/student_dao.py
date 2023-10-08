d8fa2b61a6b631080ac09d66c3b68f4efd55e531
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:06:25 +0530

    Update student_dao.py

--- dao/student_dao.py
+++ dao/student_dao.py
@@ -58,5 +58,5 @@ class StudentDAO:
         if row is None:
             return None
         return Student(
-            **dict(zip(['index_number', 'name', 'address', 'gender', 'is_admin', 'hashed_password'], row))
+            **dict(zip(['index_number', 'name', 'address', 'gender', 'date_of_birth', 'hashed_password'], row))
         )
