83b224c9892a77a1b9ee957d276a2bf3bd980851
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:09:36 +0530

    Update user_model.py

--- models/user_model.py
+++ models/user_model.py
@@ -5,7 +5,7 @@ class User(BaseModel):
     firstname: str | None = None
     lastname: str | None = None
     email: str | None = None
-    
+    contact_number: str | None = None
     is_admin: bool | None = None
 
 
