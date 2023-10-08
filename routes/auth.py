73afa8aa529751efb165305f3435179fef66134c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 14:24:14 +0530

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -106,7 +106,7 @@ async def login_for_access_token(
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires.seconds
+        data={"sub": user.email}, expires_delta=access_token_expires.seconds
         # Unresolved attribute reference 'username' for class 'bool'
     )
 
