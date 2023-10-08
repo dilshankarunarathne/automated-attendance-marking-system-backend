8d562f2537d149698be0f159270f3ec1e99498ef
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 21:53:39 +0530

    Update attendance_service.py

--- services/attendance_service.py
+++ services/attendance_service.py
@@ -3,7 +3,7 @@ from dao.student_dao import StudentDAO
 from config import config
 from models.student_model import Student
 
-dao = StudentDAO(
+dao = AttendanceDAO(
     host=config.get("database", "database.host"),
     user=config.get("database", "database.user"),
     password=config.get("database", "database.password"),
