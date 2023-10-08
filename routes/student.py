8ef707b18b120dfedaf4e8e83db96fc47786b7d6
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-10-08 15:21:07 +0530

    Update student.py

--- routes/student.py
+++ routes/student.py
@@ -12,7 +12,7 @@ router = APIRouter(
 @router.post('/register')
 async def register_new_student(
     index_number,
-        name, address, gender, date_of_birth, parent_name, contact_number, grade
+    name, address, gender, date_of_birth, parent_name, contact_number, grade
     token: str = Depends(oauth2_scheme)
 ):
     if await get_current_user(token) is None:
